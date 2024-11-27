ce035ad9922517db4761d6bbb6ef3f4b49d408ca
tetracalibers <tomix05may@gmail.com>
2024-11-28 07:43:08 +0900

    addon-interactionsによるテスト

--- src/stories/PureInboxScreen.stories.js
+++ src/stories/PureInboxScreen.stories.js
@@ -1,4 +1,5 @@
 import PureInboxScreen from './PureInboxScreen.vue'
+import { fireEvent, within } from '@storybook/test'
 
 export default {
   component: PureInboxScreen,
@@ -10,4 +11,14 @@ export const Default = {}
 
 export const Error = {
   args: { error: true }
+}
+
+export const WithInteractions = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    // Simulates pinning the first task
+    await fireEvent.click(canvas.getByLabelText('pinTask-1'))
+    // Simulates pinning the third task
+    await fireEvent.click(canvas.getByLabelText('pinTask-3'))
+  }
 }
