78b67c76259d446dfadacb3a575f00c36b46472d
tetracalibers <tomix05may@gmail.com>
2024-11-28 07:39:25 +0900

    コンポーネントから画面を構築

---
+++ src/stories/InboxScreen.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { computed } from 'vue'
+import PureInboxScreen from './PureInboxScreen.vue'
+import { useTaskStore } from './TaskList-store'
+
+// ストアのインスタンスを作成
+const store = useTaskStore()
+
+// ストアの状態からエラー状態を取得
+const isError = computed(() => store.status === 'error')
+</script>
+
+<template>
+  <PureInboxScreen :error="isError" />
+</template>
---
+++ src/stories/PureInboxScreen.vue
@@ -0,0 +1,32 @@
+<script setup>
+import TaskList from './TaskList.vue'
+
+// Propsの定義
+const props = defineProps({
+  error: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <div>
+    <!-- エラーが発生した場合の表示 -->
+    <div v-if="error" class="page lists-show">
+      <div class="wrapper-message">
+        <span class="icon-face-sad" />
+        <p class="title-message">Oh no!</p>
+        <p class="subtitle-message">Something went wrong</p>
+      </div>
+    </div>
+
+    <!-- エラーがない場合の表示 -->
+    <div v-else class="page lists-show">
+      <nav>
+        <h1 class="title-page">Taskbox</h1>
+      </nav>
+      <TaskList />
+    </div>
+  </div>
+</template>
