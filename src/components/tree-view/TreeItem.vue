c6b7cfdc869311b16b91fc58d76b7fd5cefc495d
tetracalibers <tomix05may@gmail.com>
2024-11-29 07:52:22 +0900

    tabindexを修正

--- src/components/tree-view/TreeItem.vue
+++ src/components/tree-view/TreeItem.vue
@@ -7,19 +7,21 @@ interface TreeNode {
   children?: TreeNode[]
 }
 
-const props = defineProps<{ model: TreeNode }>()
+const props = withDefaults(defineProps<{ model: TreeNode; isRoot?: boolean }>(), {
+  isRoot: true
+})
 
-const isOpen = ref(false)
+const isOpen = ref(true)
 const isFolder = computed(() => {
   return props.model.children && props.model.children.length
 })
 </script>
 
 <template>
-  <li role="treeitem" aria-selected="false" tabindex="-1">
+  <li role="treeitem" aria-selected="false" :tabindex="props.isRoot ? 0 : -1">
     {{ props.model.label }}
     <ul v-show="isOpen" v-if="isFolder" role="group">
-      <TreeItem v-for="model in model.children" :model="model" />
+      <TreeItem v-for="model in model.children" :model="model" :is-root="false" />
     </ul>
   </li>
 </template>
