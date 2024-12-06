a2754afc0c9ad0c80bd3f307868aa77addc78c27
tetracalibers <tomix05may@gmail.com>
2024-12-06 09:55:48 +0900

    storyコードを表示するようにaddonを設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ const config = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
+    '@storybook/addon-storysource'
   ],
   framework: {
     name: '@storybook/vue3-vite',
