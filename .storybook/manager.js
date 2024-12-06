ee7f139b47a1b99185b93d4fff621d381441fc6d
tetracalibers <tomix05may@gmail.com>
2024-12-07 06:13:10 +0900

    レスポンシブ確認を容易にするため、パネルを右に置く

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api'
+
+// ref: https://storybook.js.org/docs/configure/user-interface/features-and-behavior
+addons.setConfig({
+  panelPosition: 'right'
+})
