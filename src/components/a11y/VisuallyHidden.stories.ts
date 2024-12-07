fa10bfbecbd2c738ec0edfda3b0f927c976124e8
tetracalibers <tomix05may@gmail.com>
2024-12-07 13:27:03 +0900

    impl VisuallyHidden

---
+++ src/components/a11y/VisuallyHidden.stories.ts
@@ -0,0 +1,9 @@
+import VisuallyHidden from './VisuallyHidden.vue'
+
+export default {
+  component: VisuallyHidden,
+  title: 'a11y/VisuallyHidden',
+  tags: ['autodocs']
+}
+
+export const Default = {}
---
+++ src/components/a11y/VisuallyHidden.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="VisuallyHidden">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.VisuallyHidden {
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: inset(50%);
+  height: 1px;
+  width: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+}
+</style>
